fea3816010e18cf8c30583ee8ede6dfd40e28093
Bardies <bardees.maher.k@gmail.com>
2022-12-03 17:54:58 +0200

    Solve 217. Contains Duplicate in another way faster using hashtable.

--- Contains Duplicate.cs
+++ Contains Duplicate.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +9,19 @@ namespace Leet_Code
 {
     internal class Contains_Duplicate
     {
-        public bool ContainsDuplicate(int[] nums)
+        public bool ContainsDuplicateLINQ(int[] nums) //I don't know how C# implement distinct. 
         {
             return nums.Length != nums.Distinct().Count();
+        }
+        public bool ContainsDuplicate(int[] nums) //Faster in runtime andd alittle bit large in memory.
+        {
+            Hashtable hashtable = new Hashtable();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (hashtable.ContainsKey(nums[i])) return true;
+                else hashtable.Add(nums[i], 1);
+            }
+            return false;
         }
     }
 }
