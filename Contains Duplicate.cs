d52a0f27d490503ca2548faf14b37718c5622e59
Bardies <bardees.maher.k@gmail.com>
2022-12-03 17:50:01 +0200

    Solve 217. Contains Duplicate using distinct fn. in LINQ

---
+++ Contains Duplicate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leet_Code
+{
+    internal class Contains_Duplicate
+    {
+        public bool ContainsDuplicate(int[] nums)
+        {
+            return nums.Length != nums.Distinct().Count();
+        }
+    }
+}
