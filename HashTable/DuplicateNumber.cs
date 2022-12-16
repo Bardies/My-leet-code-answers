03b9c6bec39e36a5d6394f63e78f93aae875641f
Bardies <bardees.maher.k@gmail.com>
2022-12-16 18:28:39 +0200

    287. Find the Duplicate Number Solution

---
+++ HashTable/DuplicateNumber.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leet_Code.HashTable
+{
+    internal class DuplicateNumber
+    {
+        public int FindDuplicate(int[] nums)
+        {
+            Hashtable numbers = new Hashtable();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (numbers.ContainsKey(nums[i])) return nums[i];
+                numbers.Add(nums[i], i);
+            }
+            return 0;
+        }
+    }
+}
