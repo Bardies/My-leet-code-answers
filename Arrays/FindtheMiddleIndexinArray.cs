f98d424f33de61fcb3bea8511bd6631af21ae163
Bardies <bardees.maher.k@gmail.com>
2023-02-19 00:14:42 +0200

    Solve trapping rain water and find middle index in array using arrays

---
+++ Arrays/FindtheMiddleIndexinArray.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leet_Code.Arrays
+{
+    internal class FindtheMiddleIndexinArray
+    {
+        //Brute force
+        public int FindMiddleIndex(int[] nums)
+        {
+            int middleIdx = -1;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int sumBefore = 0, sumAfter = 0;
+                for (int j = i+1; j < nums.Length; j++)
+                {
+                    sumAfter += nums[j];
+                }
+                for (int k = 0; k < i; k++)
+                {
+                    sumBefore += nums[k];
+                }
+                if (sumBefore == sumAfter)
+                {
+                    middleIdx = i;
+                    break;
+                }
+            }
+            return middleIdx;
+        }
+    }
+}
---
+++ Arrays/Trapping Rain Water.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leet_Code.Arrays
+{
+    internal class Trapping_Rain_Water
+    {
+        //Time Complexity: O(n)
+        //Space Complexity: O(n)
+        //Key: Min(maxLeft, maxRight) - height[i]
+        public int Trap(int[] height)
+        {
+            int[] maxLeftHeight = new int[height.Length];
+            int[] maxRightHeight = new int[height.Length];
+            int res = 0;
+            int maxLeft = 0, maxRight = 0;
+            for (int i = 0; i < height.Length; i++)
+            {
+                maxLeft = Math.Max(maxLeft, height[i]);
+                maxRight = Math.Max(maxRight, height[height.Length-1 - i]);
+                maxRightHeight[height.Length -1- i] = maxRight;
+                maxLeftHeight[i] = maxLeft;
+            }
+            int temp;
+            for (int i = 0; i < height.Length; i++)
+            {
+                temp = Math.Min(maxRightHeight[i], maxLeftHeight[i]) - height[i];
+                if (temp > 0) res += temp;
+            }
+            return res;
+            
+        }
+    }
+}
