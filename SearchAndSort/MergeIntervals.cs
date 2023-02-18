2eafe8610a2e9c5dac0208f18f7e260efd615807
Bardies <bardees.maher.k@gmail.com>
2023-02-19 00:15:28 +0200

    Solve problems on search and sort topic

---
+++ SearchAndSort/MergeIntervals.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leet_Code.SearchAndSort
+{
+    internal class MergeIntervals
+    {
+        public int[][] Merge(int[][] intervals)
+        {
+            var sortedByFisrtVal = intervals.OrderBy(y => y[0]).ToList();
+            List<List<int>> res = new List<List<int>>();
+            res.Add(sortedByFisrtVal[0].ToList());
+            for (int i = 0; i < intervals.Length; i++)
+            {
+                if (sortedByFisrtVal[i][0] <= res[res.Count - 1][1])
+                {
+                    res[res.Count - 1][1] = Math.Max(sortedByFisrtVal[i][1], res[res.Count - 1][1]);
+                    res[res.Count - 1][0] = Math.Min(sortedByFisrtVal[i][0], res[res.Count - 1][0]);
+                }
+                else res.Add(sortedByFisrtVal[i].ToList());
+            }
+            return res.Select(a => a.ToArray()).ToArray();
+        }
+    }
+}
---
+++ SearchAndSort/Non-overlappingIntervals.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leet_Code.SearchAndSort
+{
+    internal class Non_overlappingIntervals
+    {
+        public int EraseOverlapIntervals(int[][] intervals)
+        {
+            var sortedByFisrtVal = intervals.OrderBy(y => y[0]).ToList();
+            int count = 0;
+            int previousEnd = sortedByFisrtVal[0][1];
+            for (int i = 1; i < sortedByFisrtVal.Count; i++)
+            {
+               if(sortedByFisrtVal[i][0] >= previousEnd)
+                {
+                    //no overlap
+
+                    previousEnd = sortedByFisrtVal[i][1];             }
+                else
+                {
+                    count++;
+                    previousEnd = Math.Min(previousEnd, sortedByFisrtVal[i][1]);
+                }
+
+            }
+            return count;
+        }
+    }
+}
