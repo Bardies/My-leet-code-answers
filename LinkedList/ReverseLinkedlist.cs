489f359fc21d82bbf79f05e4264613bf7c5ea835
Bardies <bardees.maher.k@gmail.com>
2023-05-07 20:18:42 +0300

    Solve resverse linked list

---
+++ LinkedList/ReverseLinkedlist.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leet_Code.LinkedList
+{
+  public class ListNode {
+      public int val;
+     public ListNode next;
+    public ListNode(int val=0, ListNode next=null) {
+         this.val = val;
+         this.next = next;
+     }
+ }
+ 
+    internal class ReverseLinkedlist
+    {
+        public ListNode ReverseList(ListNode head)
+        {
+            ListNode prev = null;
+            ListNode currentNode = head;
+            ListNode temp, next;
+            while (currentNode.next != null)
+            {
+                next = currentNode.next;
+                temp = currentNode.next;
+                currentNode.next = prev;
+                prev = temp;
+                currentNode = next;
+            }
+            return currentNode.next;
+        }
+    }
+}
